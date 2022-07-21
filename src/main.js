5459dbbccd68c0903a3b47d92aae7cc52d903b6a
Carzate <carzate@applab.mx>
2022-07-21 11:31:55 -0500

    feat: Add first 3 sections

--- src/main.js
+++ src/main.js
@@ -12,3 +12,20 @@ import './assets/fonts/Marcheile-Bold-Condensed.woff2';
  *
  */
 
+let placeholder = document.querySelector("#output")
+let out = "";
+
+for(let product of menu.menuList){
+    out +=`
+        <div class="menu-item">
+            <img src="${product.image}" width="100%">
+            <div class="menu-price">
+                <h4>${product.menuName}</h4>
+                <h4 class="color-orange">$${product.menuPrice}</h4>
+            </div>
+            <p style="width:100%">${product.menuDescription}</p>
+        </div>
+    `
+}
+placeholder.innerHTML = out
+
